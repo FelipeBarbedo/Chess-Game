e6030ca61bb710cdedf7eac5cfeae8e9c9b4bb0d
Felipe Barbedo <felipebarbedo54@gmail.com>
2023-03-22 08:51:22 -0300

    Bishop

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
@@ -196,6 +197,9 @@ public class ChessMatch {
 		placeNewPiece('E', 1, new King(board, Color.WHITE));
 		placeNewPiece('D', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('H', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('C', 1, new Bishop(board, Color.WHITE));
+		placeNewPiece('F', 1, new Bishop(board, Color.WHITE));
+
 		placeNewPiece('A', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('B', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('C', 2, new Pawn(board, Color.WHITE));
@@ -207,6 +211,9 @@ public class ChessMatch {
 
 		placeNewPiece('A', 8, new King(board, Color.BLACK));
 		placeNewPiece('B', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('C', 8, new Bishop(board, Color.BLACK));
+		placeNewPiece('F', 8, new Bishop(board, Color.BLACK));
+
 		placeNewPiece('A', 7, new Pawn(board, Color.BLACK));
 		placeNewPiece('B', 7, new Pawn(board, Color.BLACK));
 		placeNewPiece('C', 7, new Pawn(board, Color.BLACK));
